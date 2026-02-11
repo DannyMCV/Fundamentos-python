67c7b477b8f535842db774d7eef9d80000ceb08b
Daniel Castelwhite <danielmaucasvega@gmail.com>
2026-02-11 07:27:17 -0500

    Se adicionan las clases de comentarios y variables

--- sintaxis.py
+++ sintaxis.py
@@ -1,2 +1,10 @@
-if 5 > 3:
-    print("5 is greater than 3")
+
+#Esto es un comentario
+""" Esto tambien es u comentario"""
+
+if 4 > 3:
+    print("4 es mayor que 3");
+
+
+x,y,z = "Manzana","Naranja","Banana";
+print(x,y,z);
